3f9fce907e28f5741070cc685f79d2ab66a93bff
Carlosperez2 <carlosperez20131015@gmail.com>
2024-01-05 18:58:25 +0100

    refactor(app.js): listen mididied

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.status(200).send({
-    message: "Hi world!",
+    message: "¡Hola Mundo!",
   });
 });
 
-app.listen(port, `Server listening from http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server listening from http://localhost:${port}`);
+});
